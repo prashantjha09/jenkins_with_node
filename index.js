564ab468979ccedf9c8fb800702bad222cbab406
Prashant Jha <prashant@engagedly.com>
2019-09-05 11:51:44 +0530

    index.js changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('!*!*@*@*##Hello ji Prashant Jha kya hal hae!!*!');
+  res.send('!!Hello ji Prashant Jha kya hal hae!!');
 });
 
 var server = app.listen(3000, function () {
