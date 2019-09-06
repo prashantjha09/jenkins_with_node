b6b5220d550bc77908b8dd2871ec66eafcb070e5
Prashant Jha <prashant@engagedly.com>
2019-09-06 15:45:00 +0530

    index.js changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('*+++******!!*@*test*@*!!');
+  res.send('*!!*@*Hello *@*!!');
 });
 
 var server = app.listen(3000, function () {
